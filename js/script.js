100b1b2edb8707ee4e0450597c015113037211cf
Sean Cooley <scooley@snap-interactive.com>
2015-04-30 17:24:08 -0400

    style updates and add test callback to 'invite 10 friends'

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@ $(document).ready(function(){
 	
 	var score = $('.indicator').data('score');
 	var fillWidth = ((score-60)/40)*100
-	console.log(fillWidth);
+
 
 	$('.fill').width(fillWidth + '%');
-	
+
+	$('#invite').click(function(){
+		console.log('clicked!');
+		window.location.href = 'thegrade://browse';
+	});
+
+
+
 });
